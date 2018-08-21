af7a04d217251d4436de692dbdb5592b155de7cf
carlos-lls <carlos-lls@hotmail.com>
2018-08-21 10:54:35 -0300

    exercicos

---
+++ script.js
@@ -0,0 +1,19 @@
+﻿let tentativa = parseInt(prompt("Tente advinhar o número entre 0 e 100"));
+
+ let sorteio = Math.floor(Math.random() * 100 + 1);
+ 
+
+while(tentativa !== sorteio){
+
+if(sorteio > tentativa ){
+    alert("É um número maior " );
+    
+}
+
+else  {
+    alert("É um número menor");
+    
+}
+tentativa = parseInt(prompt("Tente novamente"));
+}
+alert("Você tem sorte")
